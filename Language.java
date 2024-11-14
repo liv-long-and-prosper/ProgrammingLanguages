2f19348a7fb2243d908cbfcb6ea1a5afacd745be
liv-long-and-prosper <longolivia1129@gmail.com>
2024-11-13 16:10:21 -0800

    Initial commit

---
+++ Language.java
@@ -0,0 +1,195 @@
+/**
+ * Language class to be used to interpret keyword files
+ *
+ * @author Liv Long
+ * @version 11-11-2024
+ * */
+
+package ProgrammingLanguages;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * The type Language.
+ */
+public class Language implements LanguageInterface {
+
+  // Language class attributes
+  private String name;
+  private String fileName;
+  private Type type;
+  private String[] keywords;
+
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param name     the name
+     * @param fileName the file name
+     * @param type     the type
+     */
+    public Language(String name, String fileName, Type type) {
+
+      // Constructor Pre-conditions
+      if(name == null || name.isBlank()){
+          throw new IllegalArgumentException("name cannot be null or empty, please provide a valid String for name.");
+      }
+      if(fileName == null || fileName.isBlank()){
+          throw new IllegalArgumentException("fileName cannot be null or empty, please provide a valid String for fileName.");
+      }
+      if(type == null){
+          throw new IllegalArgumentException("type cannot be null, please provide a valid Type");
+      }
+
+      this.name = name;
+      this.fileName = fileName;
+      this.type = type;
+
+
+      File kwdFile = new File(fileName);
+      Scanner fileScan = null;
+      int kwdCount;
+
+      // Prepare file to be read
+      try{
+          fileScan = new Scanner(kwdFile);
+          kwdCount = fileScan.nextInt();
+      }catch (FileNotFoundException e){
+          kwdCount = 0;
+          System.out.printf("Error: %s", e.getMessage());
+          System.out.printf("%s does not contain any key words.\n" +
+                  "If you think this is a mistake, please verify the information contained in %s and try again.\n", name, fileName);
+      }
+
+          // Initialize array with keyword count
+          keywords = new String[kwdCount];
+
+          for (int i = 0; i < keywords.length; i++) {
+              String kwd = fileScan.next();
+              keywords[i] = kwd;
+          }
+          System.out.println("Before sorting: "+Arrays.toString(keywords));
+          sortKwds();
+          System.out.println("After sorting: "+Arrays.toString(keywords));
+
+          if (fileScan != null) {
+              fileScan.close();
+          }
+  }
+
+ /**
+  * {@inheritDoc}
+  *
+  * */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * */
+    public String getFilename() {
+        return fileName;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * */
+    public Type getType() {
+        return type;
+    }
+
+    /**
+     * {@inheritDoc}
+     * */
+    public int getKwdCount() {
+        return keywords.length;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     * */
+    public String getKwd(int index) {
+      String e;
+      int maxIndex = keywords.length-1;
+      // Preconditions
+      if(index < 0){
+          e = String.format("index must not be negative, please provide a whole number between 0 & %d", maxIndex);
+          throw new IllegalArgumentException(e);
+      }else if(index >= keywords.length){
+          e = String.format("index must not be greater than %d, please provide a whole number between 0 & %d", maxIndex );
+          throw new IllegalArgumentException(e);
+      }
+        return keywords[index];
+    }
+
+    /**
+     * {@inheritDoc}
+     * */
+    public int findKwd(String keyword) {
+        // Precondition
+      if(keyword == null || keyword.isBlank()){
+          throw new IllegalArgumentException("keyword cannot be null or empty, please provide a valid String");
+      }
+       int kwdIdx = -1;
+       for (int i = 0; i < keywords.length; i++){
+           if (keywords[i] == keyword){
+               kwdIdx = i;
+           };
+       }
+       return kwdIdx;
+    }
+
+    /**
+     * {@inheritDoc}
+     * */
+    public int findShortestKwdLength() {
+      int shortestKwd = keywords[0].length();
+      for (String kwd : keywords){
+            if (kwd.length() < shortestKwd){
+                shortestKwd = kwd.length();
+            }
+        }
+        return shortestKwd;
+    }
+
+    /**
+     * {@inheritDoc}
+     * */
+    public int findLongestKwdLength() {
+        int longestKwd = keywords[0].length();
+        for (String kwd : keywords){
+            if (kwd.length() > longestKwd){
+                longestKwd = kwd.length();
+            }
+        }
+        return longestKwd;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     * */
+    public void sortKwds() {
+        for(int pass = 1; pass < keywords.length; pass++){
+            String placeholder = keywords[pass];
+            int checkPos = pass-1;
+            while(checkPos >= 0 && placeholder.compareTo(keywords[checkPos])<0){
+                keywords[checkPos+1] =  keywords[checkPos];
+                checkPos--;
+            }
+            keywords[checkPos+1] = placeholder;
+        }
+    }
+
+    public String toString(){
+      int kwdCount = getKwdCount();
+      return " Language Name: "+name+" Language Type: "+type+" Keyword count: "+kwdCount;
+    }
+
+}
+
---
+++ LanguageManager.java
@@ -0,0 +1,235 @@
+/**
+ * Language Manager class to be used to interpret language files
+ *
+ * @author Liv Long
+ * @version 11-11-2024
+ * */
+
+package ProgrammingLanguages;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * The type Language manager.
+ */
+public class LanguageManager implements LanguageManagerInterface{
+
+    private Language[] languages;
+
+    /**
+     * Instantiates a new Language manager.
+     *
+     * @param languageFile the language file
+     * @throws FileNotFoundException the file not found exception
+     */
+    public LanguageManager(File languageFile) throws FileNotFoundException{
+        if(languageFile == null){
+            throw new IllegalArgumentException("languageFile must not be null, please provide a valid File");
+        }
+
+        // initialize null Scanner object
+        Scanner languageScanner;
+
+        // Prepare file to be read; throws exception if file cannot be found
+        try {
+            languageScanner = new Scanner(languageFile);
+        } catch (FileNotFoundException e) {
+            throw e;
+        }
+
+        // Count languages
+        int languageCount = languageScanner.nextInt();
+        languages = new Language[languageCount];
+
+        // Skip column labels
+        languageScanner.nextLine();
+        languageScanner.nextLine();
+
+        for(int i = 0; i < languageCount; i++){
+            // Separate each column into separate fields
+            String line = languageScanner.nextLine();
+            String[] fields = line.split(",");
+
+            String name = fields[0];
+            String fileName = "./datafiles/"+fields[1];
+            String typeString = fields[2];
+
+            // Convert String typeString to LanguageInterface.Type
+            LanguageInterface.Type type;
+            switch (typeString){
+                case "Compiled": type = LanguageInterface.Type.COMPILED;break;
+                case "Interpreted": type = LanguageInterface.Type.INTERPRETED;break;
+                case "Scripting": type = LanguageInterface.Type.SCRIPTING;break;
+                default: type = LanguageInterface.Type.NUMERICAL_ANALYSIS;
+            }
+            Language language = new Language(name, fileName, type);
+            System.out.printf("Current language: %s\n-----------------------------\n", language.getName());
+            languages[i] = language;
+            System.out.println("Languages Array: "+Arrays.toString(languages)+"\n---------------------------------\n");
+        }
+
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * */
+    public int getLanguageCount() {
+        return languages.length;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * */
+    public Language getLanguage(int index) {
+        return languages[index];
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * */
+    public int findShortestKwdLength() {
+        int shortestKwdLength = languages[0].findShortestKwdLength();
+        for(Language language : languages){
+            if (language.findShortestKwdLength() < shortestKwdLength){
+                shortestKwdLength = language.findLongestKwdLength();
+            }
+        }
+        return shortestKwdLength;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * */
+    public int findLongestKwdLength() {
+        int longestKwdLength = languages[0].findLongestKwdLength();
+        for(Language language : languages){
+            if (language.findLongestKwdLength() > longestKwdLength){
+                longestKwdLength = language.findLongestKwdLength();
+            }
+        }
+        return longestKwdLength;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * */
+    public int findLangWithFewestKwds() {
+        int langIndex = -1;
+        int fewestKwds = languages[0].getKwdCount();
+
+        for (int i = 0; i < languages.length; i++) {
+            int currentKwdCount = languages[i].getKwdCount();
+            if (currentKwdCount > 0) {
+                if (fewestKwds == 0 || currentKwdCount < fewestKwds) {
+                    fewestKwds = currentKwdCount;
+                    langIndex = i;
+                }
+            }
+        }
+        return langIndex;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * */
+    public int findLangWithMostKwds(){
+        int langIndex = -1;
+        int mostKwds = 0;
+
+        for(int i = 0; i < languages.length; i ++){
+            int currentKwdCount = languages[i].getKwdCount();
+            if (currentKwdCount > mostKwds){
+                mostKwds = currentKwdCount;
+                langIndex = i;
+            }
+        }
+        return langIndex;
+        }
+
+
+        /**
+         * {@inheritDoc}
+         *
+         * */
+    public int[] findLangKwdMatches(String keyword) {
+        // initialize a null array and counter for tracking how many languages have a match
+        int[] langsWithMatches;
+        int matchCount = 0;
+
+        // Count how many languages have a match
+        for(Language lang : languages){
+            if(lang.findKwd(keyword) != -1){
+                matchCount++;
+            }
+        }
+
+        // initialize null array to be an array with length of matchCount
+        langsWithMatches = new int[matchCount];
+        int lastMatchIndex = -1;
+
+        for (int i = 0; i < langsWithMatches.length; i++){
+            for(int j = 0; j < languages.length; j++){
+                int kwdFound = languages[j].findKwd(keyword);
+                if (kwdFound != -1 && j > lastMatchIndex){
+                    lastMatchIndex = j;
+                    langsWithMatches[i] = j;
+                }
+            }
+        }
+        return langsWithMatches;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * */
+    public int[] findLangsOfType(Language.Type type) {
+        int[] langsOfType;
+        int matchCount = 0;
+
+        for(Language lang : languages){
+            if(lang.getType() == type){
+                matchCount++;
+            }
+        }
+
+        langsOfType = new int[matchCount];
+
+        for(int i = 0; i < langsOfType.length; i++){
+            int langsIndex = 0;
+            for(Language lang : languages){
+                if(lang.getType() == type){
+                    langsOfType[i] = langsIndex;
+                }
+                langsIndex++;
+            }
+        }
+
+        return langsOfType;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     *
+     * */
+    public void sortLangs() {
+        for(int pass = 1; pass < languages.length; pass++){
+            Language placeholder = languages[pass];
+            int checkPos = pass-1;
+            while(checkPos >= 0 && placeholder.getName().compareTo(languages[checkPos].getName())<0){
+                languages[checkPos+1] =  languages[checkPos];
+                checkPos--;
+            }
+            languages[checkPos+1] = placeholder;
+        }
+
+    }
+}
